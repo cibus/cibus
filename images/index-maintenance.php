8a5e780bed92dd300ffd09bb60f164f9e6465168
cibus <57238195+cibus@users.noreply.github.com>
2019-10-31 21:45:03 -0400

    Add files via upload

---
+++ images/index-maintenance.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+$page='index';
+include("header.php");
+include("db.php");
+?>
+	
+	
+  </div>
+
+
+	<div class='container'>
+		<div class="row">		
+			<div class="span12">
+			<div class='greenoutline'>
+			<img style='width:100%;height:100%;' src='images/header.jpg' /></div>
+			</div>
+			</div>  
+	</div>
+
+	<div class='container'>
+		<div class="row">
+			<div class="span12">
+				<p class="text-center">
+					<span style='color:#124F00;font-size:35pt;height:100px;background-color:;line-height:100px;font-weight:bold;'>Leading Industrial Cooking Technology</span>
+				</p>
+			</div>
+		</div> 
+	</div>
+
+
+	<div class='container'>
+		<div class="row">		
+			<div class="span7">
+				<p>A paragraph is a collection of related sentences dealing with a single topic. Learning to write good paragraphs will help you as a writer stay on track during your drafting and revision stages. Good paragraphing also greatly assists your readers in following a piece of writing. You can have fantastic ideas, but if those ideas aren't presented in an organized fashion, you will lose your readers (and fail to achieve your goals in writing).</p>
+				<p>To be as effective as possible, a paragraph should contain each of the following: Unity, Coherence, A Topic Sentence, and Adequate Development. As you will see, all of these traits overlap. Using and adapting them to your individual purposes will help you construct effective paragraphs.</p>
+				</div>
+			<!--<div class='span1'></div>-->
+			<div class="span5">
+				<img  src='images/slider1.jpg' width="100%" height="100%" />
+			</div>
+		</div>  
+	</div>
+
+
+	<div class='container'>
+		<div class="row">		
+			<div class="span4"></div>
+			<div class="span4"></div>
+			<div class="span4"></div>
+		</div>  
+	</div>
+
+
+
+
+ 
+	
+	
+	
+	
+	
+	
+<?php
+include("footer.php");
+?>
+	
+  </body>
+</html>
